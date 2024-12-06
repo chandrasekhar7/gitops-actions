ba51eec6256b597fdb957ee7c3c6876943a7f31b
seunayolu <seunayolu@gmail.com>
2024-12-06 22:56:34 +0100

    iac-gitops-action

---
+++ terraform/eks-vpc.tf
@@ -0,0 +1,68 @@
+# VPC for Cluster
+data "aws_availability_zones" "azs" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.name
+  cidr = var.vpc_cidr_block
+
+  azs             = data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = var.tags
+}
+
+# EKS Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.2"
+
+  cluster_name                   = var.name
+  cluster_version                = var.k8s_version
+  cluster_endpoint_public_access = true
+  
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.public_subnets
+
+  create_cluster_security_group = false
+  create_node_security_group    = false
+  
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+  
+  eks_managed_node_groups = {
+    initial = {
+      instance_types = ["t2.micro"]
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+    }
+  }
+
+  tags = var.tags
+}
